5d5e4082339a812e482261898757f5027d7d76ac
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-21 22:31:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,6 @@ require 'pry'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/Kickstarter.html')
-  Kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
   binding.py
 end
